1c9c766449539586c76e9acaa7aa114587776375
brendan.salmond@propylon.com <brendan.salmond@propylon.com>
2025-09-14 20:37:55 +0100

    Add apollo sandbox
    Add apollo sandbox to deployment.

--- server.js
+++ server.js
@@ -10,11 +10,40 @@ async function startServer() {
     typeDefs,
     resolvers,
     introspection: true,
-    playground: true
+    plugins: [
+      require('apollo-server-core').ApolloServerPluginLandingPageGraphQLPlayground({
+        // Apollo Studio Sandbox will be embedded
+        settings: {
+          'editor.theme': 'dark',
+          'editor.cursorShape': 'line',
+          'editor.fontFamily': '"Source Code Pro", "Consolas", "Inconsolata", "Droid Sans Mono", "Monaco", monospace',
+          'editor.fontSize': 14,
+          'editor.reuseHeaders': true,
+          'editor.fontFamily': '"Source Code Pro", "Consolas", "Inconsolata", "Droid Sans Mono", "Monaco", monospace',
+          'general.betaUpdates': false,
+          'prettier.printWidth': 80,
+          'prettier.tabWidth': 2,
+          'prettier.useTabs': false,
+          'request.credentials': 'omit',
+          'schema.polling.enable': true,
+          'schema.polling.endpointFilter': '*localhost*',
+          'schema.polling.interval': 2000,
+          'schema.disableComments': true,
+          'tracing.hideTracingResponse': true
+        }
+      })
+    ]
   });
 
   await server.start();
-  server.applyMiddleware({ app, path: '/graphql' });
+  server.applyMiddleware({
+    app,
+    path: '/graphql',
+    cors: {
+      origin: true,
+      credentials: true
+    }
+  });
 
   const PORT = process.env.PORT || 4000;
 
